632e5479d145e8577333a8c9e0747841e62fe0e4
powershell <powershell143@gmail.com>
2024-04-17 17:58:27 +0530

    first22

--- hello.tf
+++ hello.tf
@@ -4,14 +4,7 @@ provider "aws" {
   region = "ap-south-1"
   profile = "naga"
 }
-data "aws_ami" "myami" {
- owners = ["amazon"] 
- most_recent = true
- filter {
-   name = "name"
-   values = ["Windows_Server-2022-*"]
- }
-}
+
 resource "aws_vpc" "myvpc" {
   cidr_block = var.mycidr
 
@@ -22,7 +15,14 @@ resource "aws_subnet" "mysubnet" {
     depends_on = [ aws_vpc.myvpc ]
 
 }
-
+data "aws_ami" "myami" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["al2023-ami"]
+  }
+}
 resource "aws_instance" "myinstance" {
     ami = data.aws_ami.myami.id
     instance_type = "t2.medium"
